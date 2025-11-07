a253b9053002c8dce85971bc43b632e52b6d12bb
Garima <garimadixitstp@gmail.com>
2025-11-07 12:37:31 +0530

    changes made for deployment2222

--- server/index.js
+++ server/index.js
@@ -3,10 +3,10 @@ const http = require("http");
 const app = require("./app");
 const logger = require("./utils/logger");
 
+const PORT = process.env.PORT || 10000;
+
 const server = http.createServer(app);
 
-const PORT = process.env.PORT || 3001;
-
 server.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`);
+  logger.info(`✅ Server running on port ${PORT}`);
 });
